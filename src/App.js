2fda92551a080374897f6322f091f1ffb496e133
Amro Eid <amroeidd@gmail.com>
2023-12-13 22:29:23 +0200

    I revieved the main

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 import Footer from './components/Footer';
 
+
 function App() {
   return (
     <div className="App">
+      
       <Footer/>
     </div>
   );
