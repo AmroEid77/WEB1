13e86da63e47f72c8f41055d7140cdf12d1d330c
raghadabdaljawwad@gmail.com <raghadabdaljawwad@gmail.com>
2023-12-19 19:14:40 +0200

    add Navbar style.css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Footer from './components/Footer/Footer.js';
+import Navbar from './Navbar';
+import'./style.css'
 
 
 function App() {
   return (
     <div className="App">
-      
+        <Navbar/>
       <Footer/>
     </div>
   );
