9a0c978f06aa45859e2c0b6985eec79a7e1eea3f
lama <lamashraim2004@gmail.com>
2023-12-19 16:23:12 +0200

    added the header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Footer from './components/Footer/Footer.js';
+import Header from './components/Footer/Header.js';
 
 
 function App() {
   return (
     <div className="App">
-      
+      <Header />
       <Footer/>
     </div>
   );
